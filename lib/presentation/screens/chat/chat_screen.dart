c0876cfd545ce869b4ad752eaf92761a46c686e7
aakross <royal9619@gmail.com>
2026-02-08 20:35:27 -0600

    vista terminada

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:yes_no_app2/presentation/widgets/chat/her_message_bubble.dart';
 import 'package:yes_no_app2/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:yes_no_app2/presentation/widgets/shared/message_field_box.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -40,7 +41,8 @@ class _ChatView extends StatelessWidget {
                     : const HerMessageBubble();
               },
             )),
-            const Text('Mundo')
+            //Caja de texto de mensajes
+            MessageFieldBox()
           ],
         ),
       ),
